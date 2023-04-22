4bf24a173966472abc732ee415957efb163d8100
Thibault Zanini <thibaultzanini@gmail.com>
2023-04-22 21:02:43 +0200

    feat: performance improvements

--- packages/settings/lib/ui/builders/theme.builder.dart
+++ packages/settings/lib/ui/builders/theme.builder.dart
@@ -1,40 +1,28 @@
 import 'package:fastyle_settings/fastyle_settings.dart';
-import 'package:tbloc/tbloc.dart';
 import 'package:flutter/material.dart';
+import 'package:tbloc/tbloc.dart';
 
-/// The [FastSettingsThemeBuilder] class is a [StatefulWidget] that listens
-/// to the [FastSettingsBloc] and builds its child widget when the theme
-/// mode changes.
-class FastSettingsThemeBuilder extends StatefulWidget {
+/// A widget that listens to changes in the `FastSettingsBloc` and rebuilds its
+/// child widget when the theme mode changes.
+class FastSettingsThemeBuilder extends StatelessWidget {
   /// The builder function that will be called when the theme mode changes.
   final BlocBuilder<FastSettingsBlocState> builder;
 
+  /// Creates a new `FastSettingsThemeBuilder` instance.
+  ///
+  /// The `builder` parameter is a required function that takes the
+  /// [BuildContext] and [FastSettingsBlocState] as input and returns a widget.
   const FastSettingsThemeBuilder({
-    super.key,
+    Key? key,
     required this.builder,
-  });
-
-  @override
-  State<FastSettingsThemeBuilder> createState() =>
-      _FastSettingsThemeBuilderState();
-}
-
-class _FastSettingsThemeBuilderState extends State<FastSettingsThemeBuilder>
-    with FastSettingsThemeMixin {
-  late final FastSettingsBloc _settingsBloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _settingsBloc = BlocProvider.of<FastSettingsBloc>(context);
-  }
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilderWidget(
       buildWhen: (previous, next) => previous.theme != next.theme,
-      builder: widget.builder,
-      bloc: _settingsBloc,
+      bloc: BlocProvider.of<FastSettingsBloc>(context),
+      builder: builder,
     );
   }
 }
