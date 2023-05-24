032ec218898954f8aa0a71a151b4926372f2a4d6
Thibault Zanini <thibaultzanini@gmail.com>
2023-05-24 18:37:02 +0200

    feat: add amountSwitchFieldTypeFromString helper

--- packages/forms/lib/logic/enums/amount_switch_field_type.enum.dart
+++ packages/forms/lib/logic/enums/amount_switch_field_type.enum.dart
@@ -2,3 +2,14 @@ enum FastAmountSwitchFieldType {
   percent,
   amount,
 }
+
+FastAmountSwitchFieldType? amountSwitchFieldTypeFromString(String? string) {
+  switch (string) {
+    case 'percent':
+      return FastAmountSwitchFieldType.percent;
+    case 'amount':
+      return FastAmountSwitchFieldType.amount;
+    default:
+      return null;
+  }
+}
