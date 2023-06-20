ad53bca7365ab793db60e275d352a6101fd6099c
Thibault Zanini <thibaultzanini@gmail.com>
2023-06-21 00:08:12 +0200

    feat: add FastCalculatorCategoryResult

---
+++ packages/calculator/lib/ui/widgets/category_result.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fastyle_dart/fastyle_dart.dart';
+
+/// A widget that displays the result of a calculator category.
+class FastCalculatorCategoryResult extends StatelessWidget {
+  /// The text to display in the result.
+  final String text;
+
+  /// Creates a new [FastCalculatorCategoryResult] instance.
+  const FastCalculatorCategoryResult({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        kFastSizedBox12,
+        Container(
+          decoration: ThemeHelper.createBorderSide(context),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FastSubtitle(text: text),
+              kFastSizedBox8,
+            ],
+          ),
+        ),
+        kFastSizedBox12,
+      ],
+    );
+  }
+}
--- packages/calculator/lib/ui/widgets/widgets.dart
+++ packages/calculator/lib/ui/widgets/widgets.dart
@@ -3,3 +3,4 @@ export './clear_action.widget.dart';
 export './refresh_action.widget.dart';
 export './share_action.widget.dart';
 export './info_action.widget.dart';
+export './category_result.dart';
