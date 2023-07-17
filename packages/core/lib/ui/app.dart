14f467b7e692cac8ea50ce49550e4803354304c0
Thibault Zanini <thibaultzanini@gmail.com>
2023-07-17 18:11:52 +0200

    chore: more comments

--- packages/core/lib/ui/app.dart
+++ packages/core/lib/ui/app.dart
@@ -12,26 +12,80 @@ import 'package:fastyle_core/fastyle_core.dart';
 
 /// The main entry point for a Fastyle Dart application.
 class FastApp extends StatefulWidget {
+  /// A list of [BlocProviderSingleChildWidget] to provide the [Bloc] instances
+  /// to the application.
   final List<BlocProviderSingleChildWidget>? blocProviders;
+
+  /// A callback function that is invoked when the database version is changed.
   final DatabaseVersionChanged? onDatabaseVersionChanged;
+
+  /// A key to the root navigator state that can be used to perform
+  /// navigation actions from anywhere in the application.
   final GlobalKey<NavigatorState>? rootNavigatorKey;
+
+  /// A builder function that builds an error widget for displaying errors
+  /// during the loading process of the application.
   final FastAppLoaderErrorBuilder? errorBuilder;
+
+  /// A builder function that builds the loading widget that is shown
+  /// during the initialization of the application.
   final FastAppLoaderBuilder? loaderBuilder;
+
+  /// The duration to delay before showing the loading widget during
+  /// the initialization of the application.
   final Duration delayBeforeShowingLoader;
+
+  /// An error reporter implementation for reporting errors that occur
+  /// during the application lifecycle.
   final IFastErrorReporter? errorReporter;
+
+  /// A builder function that builds the onboarding widget that is shown
+  /// to the user when the application is launched for the first time.
   final WidgetBuilder? onboardingBuilder;
+
+  /// A flag indicating whether to show the debug banner on the top
+  /// right corner of the screen.
   final bool debugShowCheckedModeBanner;
+
+  /// A list of [FastJob] instances to be executed during the
+  /// loading process of the application.
   final Iterable<FastJob>? loaderJobs;
+
+  /// An [FastAppInfoDocument] instance that contains information about the
+  /// application such as name, version, supported locales, etc.
   final FastAppInfoDocument appInfo;
+
+  /// A flag indicating whether to force the onboarding process even
+  /// if the application has been launched before.
   final bool forceOnboarding;
+
+  /// A builder function that builds the home widget that is shown to the
+  /// user when the application finishes loading.
   final WidgetBuilder? homeBuilder;
+
+  /// The path to the localization assets directory.
   final String localizationPath;
+
+  /// A list of custom routes for the application.
   final List<RouteBase> routes;
+
+  /// The fallback locale to use if the system locale is not supported.
   final Locale fallbackLocale;
+
+  /// The light theme data for the application.
   final ThemeData? lightTheme;
+
+  /// The initial route to navigate to when the application starts.
   final String? initialRoute;
+
+  /// The dark theme data for the application.
   final ThemeData? darkTheme;
+
+  /// The dynamic asset loader for the localization assets.
   final dynamic assetLoader;
+
+  /// A flag indicating whether to ask the user for an app review
+  /// when certain conditions are met.
   final bool askForReview;
 
   const FastApp({
@@ -137,7 +191,7 @@ class _FastAppState extends State<FastApp> {
         errorBuilder: widget.errorBuilder,
         appBuilder: buildAppOrOnboarding,
         loaderJobs: [
-          // FIXME:  onDatabaseVersionChanged should be called once the app
+          // FIXME: onDatabaseVersionChanged should be called once the app
           // has been initialized.
           FastAppInfoJob(
             widget.appInfo,
@@ -160,7 +214,7 @@ class _FastAppState extends State<FastApp> {
 
     if (appInfo.isFirstLaunch || widget.forceOnboarding) {
       // FIXME: add a property to the Fast app info to define
-      // if has been already initialized or not.
+      // if it has been already initialized or not.
       if (widget.onboardingBuilder != null) {
         return Builder(builder: widget.onboardingBuilder!);
       }
