ace999b840685ad40dd7a8896a1ab251502208a0
Thibault Zanini <thibaultzanini@gmail.com>
2023-07-20 15:28:52 +0200

    chore: add buttons example

---
+++ packages/buttons/example/lib/main.dart
@@ -0,0 +1,69 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:fastyle_core/fastyle_core.dart';
+import 'package:fastyle_dart/fastyle_dart.dart' hide FastApp;
+import 'package:fastyle_buttons/fastyle_buttons.dart';
+import 'package:lingua_core/lingua_core.dart';
+import 'package:lingua_core/generated/codegen_loader.g.dart';
+
+final kAppInfo = kFastAppInfo.copyWith(
+  appName: 'Fastyle Buttons',
+  databaseVersion: 0,
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isPending = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FastApp(
+      appInformation: kAppInfo,
+      homeBuilder: (context) => buildHome(context),
+      assetLoader: const LinguaLoader(
+        mapLocales: CoreCodegenLoader.mapLocales,
+        // FIXME
+      ),
+    );
+  }
+
+  Widget buildHome(BuildContext context) {
+    return FastSectionPage(
+      titleText: 'Buttons',
+      showAppBar: false,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const FastCopyButton(valueText: '42'),
+          FastPendingOutlineButton(
+            text: 'Outline button',
+            onTap: () {
+              setState(() => _isPending = !_isPending);
+            },
+            isPending: _isPending,
+          ),
+          FastPendingRaisedButton(
+            text: 'Raised button',
+            onTap: () {
+              setState(() => _isPending = !_isPending);
+            },
+            isPending: _isPending,
+          ),
+        ],
+      ),
+    );
+  }
+}
