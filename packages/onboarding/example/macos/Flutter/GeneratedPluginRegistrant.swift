64a6556925fc20f599129cc7c70fcbff9578aaf7
Thibault Zanini <thibaultzanini@gmail.com>
2023-08-09 19:25:25 +0200

    chore: update example dependencies

--- packages/onboarding/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/onboarding/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,10 @@ import Foundation
 import devicelocale
 import firebase_core
 import firebase_messaging
+import in_app_purchase_storekit
 import package_info_plus
 import path_provider_foundation
+import printing
 import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
@@ -18,8 +20,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
