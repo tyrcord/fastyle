fd1f2a45bbffac73a8414961cb10a929d6c956ec
Thibault Zanini <thibaultzanini@gmail.com>
2023-08-29 16:48:40 +0200

    perf: simplify FastCalculatorCategoryResult

--- packages/calculator/lib/ui/widgets/category_result.dart
+++ packages/calculator/lib/ui/widgets/category_result.dart
@@ -9,30 +9,30 @@ class FastCalculatorCategoryResult extends StatelessWidget {
   /// The text to display in the result.
   final String text;
 
+  final String? value;
+
+  final Color? borderColor;
+
   /// Creates a new [FastCalculatorCategoryResult] instance.
   const FastCalculatorCategoryResult({
     super.key,
     required this.text,
+    this.value,
+    this.borderColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        kFastSizedBox12,
-        DecoratedBox(
-          decoration: ThemeHelper.createBorderSide(context),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              FastSubtitle(text: text),
-              kFastSizedBox8,
-            ],
-          ),
-        ),
-        kFastSizedBox12,
-      ],
+    final colors = ThemeHelper.colors;
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+      decoration: ThemeHelper.createBorderSide(
+        context,
+        color: borderColor,
+      ),
+      padding: const EdgeInsets.only(bottom: 6.0),
+      child: FastSubtitle(text: text),
     );
   }
 }
