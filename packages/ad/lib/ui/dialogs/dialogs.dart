e958d921ccee54039985f564d0b18e2d1f883e39
Thibault Zanini <thibaultzanini@gmail.com>
2023-09-18 10:30:08 +0200

    fix: remove fastyle_ad dependency from calculators

--- packages/ad/lib/ui/dialogs/dialogs.dart
+++ packages/ad/lib/ui/dialogs/dialogs.dart
@@ -6,7 +6,10 @@ import 'package:fastyle_core/fastyle_core.dart';
 
 // Project imports:
 import './operation_rewared.dialog.dart';
+import './export_pdf.dialog.dart';
 
+// Exports:
+export './export_pdf.dialog.dart';
 export './operation_rewared.dialog.dart';
 
 void showOperationRewardedDialog({
@@ -35,6 +38,54 @@ void showOperationRewardedDialog({
     context: context,
     barrierDismissible: barrierDismissible,
     child: FastOperationRewardedDialog(
+      onGetTitleText: onGetTitleText,
+      onGetCancelText: onGetCancelText,
+      onGetValidText: onGetValidText,
+      onCreateOperation: onCreateOperation,
+      intialBuilder: intialBuilder,
+      onOperationStatusChanged: onOperationStatusChanged,
+      onGrantRights: onGrantRights,
+      onVerifyRights: onVerifyRights,
+      verifyingRightsBuilder: verifyingRightsBuilder,
+      grantingRightsBuilder: grantingRightsBuilder,
+      rightsDeniedBuillder: rightsDeniedBuillder,
+      operationInProgressBuilder: operationInProgressBuilder,
+      operationSucceededBuilder: operationSucceededBuilder,
+      operationFailedBuilder: operationFailedBuilder,
+      missingRightsBuilder: missingRightsBuilder,
+      errorBuilder: errorBuilder,
+      onCancel: onCancel,
+      onValid: onValid,
+    ),
+  );
+}
+
+void showRewardedExportPdfDialog({
+  required BuildContext context,
+  required FutureBoolCallback onCreateOperation,
+  WidgetBuilder? intialBuilder,
+  String? Function(FastOperationStatus)? onGetValidText,
+  String? Function(FastOperationStatus)? onGetCancelText,
+  String? Function(FastOperationStatus)? onGetTitleText,
+  Widget Function(BuildContext context, dynamic error)? errorBuilder,
+  bool barrierDismissible = false,
+  FastOperationStatusChanged? onOperationStatusChanged,
+  FutureBoolCallback? onVerifyRights,
+  FutureBoolCallback? onGrantRights,
+  WidgetBuilder? verifyingRightsBuilder,
+  WidgetBuilder? grantingRightsBuilder,
+  WidgetBuilder? rightsDeniedBuillder,
+  WidgetBuilder? operationInProgressBuilder,
+  WidgetBuilder? operationSucceededBuilder,
+  WidgetBuilder? operationFailedBuilder,
+  WidgetBuilder? missingRightsBuilder,
+  FastOperationStatusChanged? onCancel,
+  FastOperationStatusChanged? onValid,
+}) {
+  showAnimatedFastOverlay(
+    context: context,
+    barrierDismissible: barrierDismissible,
+    child: FastExportPdfRewardedDialog(
       onGetTitleText: onGetTitleText,
       onGetCancelText: onGetCancelText,
       onGetValidText: onGetValidText,
--- packages/calculator/lib/ui/ui.dart
+++ packages/calculator/lib/ui/ui.dart
@@ -1,3 +1,2 @@
 export './layouts/layouts.dart';
 export './widgets/widgets.dart';
-export './dialogs/dialogs.dart';
