40655b7801a665c63d5add7daef26e485950d28c
Thibault Zanini <thibaultzanini@gmail.com>
2023-10-27 12:11:03 +0200

    feat: add missing chart colors

--- packages/charts/lib/logic/constants.dart
+++ packages/charts/lib/logic/constants.dart
@@ -8,11 +8,9 @@ const kFastChartMintColor = Color(0xFF80DAD6);
 const kFastChartTealColor = Color(0xFF7AD2EB);
 const kFastChartBlueColor = Color(0xFF80B4E6);
 const kFastChartGrayColor = Color(0xFFB8B8B8);
-
-// TODO:
-const kFastChartGreenColor = Color(0xFF69995D);
-const kFastChartOrangeColor = Color(0xFFE08119);
-const kFastChartRedColor = Color(0xFFD14334);
-const kFastChartYellowColor = Color(0xFFF2C057);
-const kFastChartBrownColor = Color(0xFFD8BBAE);
-const kFastChartPinkColor = Color(0xFFE6B8C8);
+const kFastChartGreenColor = Color(0xFF8BC990);
+const kFastChartOrangeColor = Color(0xFFFFAB7D);
+const kFastChartRedColor = Color(0xFFE68080);
+const kFastChartYellowColor = Color(0xFFF6D680);
+const kFastChartBrownColor = Color(0xFFB89E82);
+const kFastChartPinkColor = Color(0xFFF7A3C2);
