2b253617e8f81ab395a1145674564e8797d0e602
Thibault Zanini <thibaultzanini@gmail.com>
2023-11-02 23:42:56 +0100

    chore: clean up FastFadeTransitionPage

--- packages/core/lib/ui/widgets/transitions/fade_transition_page.dart
+++ packages/core/lib/ui/widgets/transitions/fade_transition_page.dart
@@ -4,24 +4,38 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:go_router/go_router.dart';
 
-//TODO: @need-review: code from fastyle_dart
-
+/// `FastFadeTransitionPage` is a widget that extends the functionality of
+/// a `CustomTransitionPage` to provide a fade transition effect.
+/// This effect is driven by a linear curve to fade in and out quickly.
 class FastFadeTransitionPage extends CustomTransitionPage<void> {
-  FastFadeTransitionPage({
-    required super.key,
+  // Constructor
+  const FastFadeTransitionPage({
+    super.key,
     required super.child,
-  }) : super(
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) =>
-              FadeTransition(
-            opacity: animation.drive(_curveTween),
-            child: child,
-          ),
-        );
+  }) : super(transitionsBuilder: _fadeTransitionBuilder);
 
-  static final CurveTween _curveTween = CurveTween(curve: Curves.linear);
+  // A CurveTween with a linear curve. This provides a linear transition effect.
+  static final CurveTween _linearCurveTween = CurveTween(curve: Curves.linear);
+
+  /// Builds the fade transition effect.
+  ///
+  /// Parameters:
+  /// - [context]: The build context.
+  /// - [animation]: The primary animation.
+  /// - [secondaryAnimation]: The secondary animation (not used here).
+  /// - [child]: The widget that should receive the transition.
+  ///
+  /// Returns:
+  /// - A widget with a fade transition effect.
+  static Widget _fadeTransitionBuilder(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return FadeTransition(
+      opacity: animation.drive(_linearCurveTween),
+      child: child,
+    );
+  }
 }
