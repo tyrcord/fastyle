10cc42a84fb743920b7c37b5e264c4a376b80a3c
Thibault Zanini <thibaultzanini@gmail.com>
2023-11-18 17:21:45 +0100

    feat: added FastFiancialAmountSwitchField

---
+++ packages/financial/lib/logic/enums/amount_switch_field_menu_option.enum.dart
@@ -0,0 +1,6 @@
+enum FastFinancialAmountSwitchMenuOption {
+  amount,
+  percent,
+  pip,
+  price,
+}
--- packages/financial/lib/ui/fields/fields.dart
+++ packages/financial/lib/ui/fields/fields.dart
@@ -1,2 +1,3 @@
 export 'position_size_switch.field.dart';
 export 'select_instrument_field.widget.dart';
+export 'amount_switch.field.dart';
