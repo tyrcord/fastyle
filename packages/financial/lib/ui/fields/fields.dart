96769b84143142853129b64d3626fb31a86ea78f
Thibault Zanini <thibaultzanini@gmail.com>
2023-12-10 18:24:22 +0100

    feat: added FastFinancialSelectLeverageField

--- packages/financial/lib/ui/fields/fields.dart
+++ packages/financial/lib/ui/fields/fields.dart
@@ -1,4 +1,5 @@
 export 'position_size_switch.field.dart';
-export 'select_instrument_field.widget.dart';
+export 'select_instrument.field.dart';
 export 'amount_switch.field.dart';
 export 'select_frequency.field.dart';
+export 'select_leverage.field.dart';
