3e2239f39d61d5a9ac20f993cec76dc1c3975347
GitHub Action <actions@github.com>
2023-12-11 01:14:04 +0000

    chore: sorted imports

--- packages/financial/lib/ui/fields/select_leverage.field.dart
+++ packages/financial/lib/ui/fields/select_leverage.field.dart
@@ -1,8 +1,11 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:collection/collection.dart' show IterableExtension;
-import 'package:lingua_finance/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fastyle_core/fastyle_core.dart';
-import 'package:flutter/material.dart';
+import 'package:lingua_finance/generated/locale_keys.g.dart';
 import 'package:matex_financial/financial.dart';
 import 'package:tbloc/tbloc.dart';
 
