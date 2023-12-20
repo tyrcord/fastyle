44008a1174ca57abc74a1dd856a8ec09ab65900d
Thibault Zanini <thibaultzanini@gmail.com>
2023-12-20 14:33:41 +0100

    feat: minor improvements

--- packages/calculator/lib/ui/overlays/instructions.overlay.dart
+++ packages/calculator/lib/ui/overlays/instructions.overlay.dart
@@ -10,17 +10,20 @@ import 'package:lingua_core/generated/locale_keys.g.dart';
 class FastCalculatorInstructionsOverlay extends StatelessWidget {
   final FastPaletteScheme? palette;
   final Widget? content;
+  final String? titleText;
 
   const FastCalculatorInstructionsOverlay({
     super.key,
     this.palette,
     this.content,
+    this.titleText,
   });
 
   @override
   Widget build(BuildContext context) {
     return FastAlertDialog(
-      titleText: CalculatorLocaleKeys.calculator_title_instructions.tr(),
+      titleText:
+          titleText ?? CalculatorLocaleKeys.calculator_title_instructions.tr(),
       validText: CoreLocaleKeys.core_label_ok.tr(),
       onValid: () => Navigator.pop(context),
       children: content != null ? [content!] : null,
