7efc9edc48f8fbb5f486a628e3eab764293dc10c
Thibault Zanini <thibaultzanini@gmail.com>
2024-01-10 18:53:07 +0100

    chore: update compile svg script

--- scripts/compile_svg.py
+++ scripts/compile_svg.py
@@ -2,12 +2,20 @@ import os
 import subprocess
 
 
+def optimize_svg_with_svgo(svg_path):
+    # Run svgo to optimize the SVG file in-place
+    subprocess.run(["svgo", svg_path])
+
+
 def convert_svg_to_vec(folder_path):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith(".svg"):
                 svg_path = os.path.join(root, file)
                 vec_path = svg_path + '.vec'
+
+                # Optimize the SVG file with svgo
+                optimize_svg_with_svgo(svg_path)
 
                 # Run the command to convert the SVG file
                 subprocess.run(
