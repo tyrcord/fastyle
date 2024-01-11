d7fe3520bdce747944dd549cff283c2edd11b991
GitHub Action <actions@github.com>
2024-01-11 01:14:23 +0000

    chore: sorted imports

--- packages/animation/lib/ui/loaders/tyrcord.loader.dart
+++ packages/animation/lib/ui/loaders/tyrcord.loader.dart
@@ -1,8 +1,13 @@
-import 'package:fastyle_animation/fastyle_animation.dart';
-import 'package:fastyle_core/fastyle_core.dart';
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:easy_localization/easy_localization.dart';
+import 'package:fastyle_core/fastyle_core.dart';
 import 'package:lingua_core/generated/locale_keys.g.dart';
+
+// Project imports:
+import 'package:fastyle_animation/fastyle_animation.dart';
 
 class FastTyrcordLoader extends StatelessWidget {
   const FastTyrcordLoader({super.key});
