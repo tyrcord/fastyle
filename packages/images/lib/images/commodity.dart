01d927cc18b916eddab270e161c43f181163f6b3
Thibault Zanini <thibaultzanini@gmail.com>
2024-01-14 14:41:15 +0100

    feat: update images

--- packages/images/lib/images/commodity.dart
+++ packages/images/lib/images/commodity.dart
@@ -5,7 +5,16 @@ class FastImageCommodity {
   static const oil = 'assets/images/commodity/oil.svg.vec';
   static const silver = 'assets/images/commodity/silver.svg.vec';
   static const gold = 'assets/images/commodity/gold.svg.vec';
+  static const coffee = 'assets/images/commodity/coffee.svg.vec';
+  static const soybeanOil = 'assets/images/commodity/soybean_oil.svg.vec';
   static const platinum = 'assets/images/commodity/platinum.svg.vec';
+  static const wheat = 'assets/images/commodity/wheat.svg.vec';
+  static const corn = 'assets/images/commodity/corn.svg.vec';
+  static const palladium = 'assets/images/commodity/palladium.svg.vec';
+  static const cotton = 'assets/images/commodity/cotton.svg.vec';
+  static const sugar = 'assets/images/commodity/sugar.svg.vec';
+  static const cocoa = 'assets/images/commodity/cocoa.svg.vec';
+  static const copper = 'assets/images/commodity/copper.svg.vec';
 }
 
 const kFastImageCommodity = [
@@ -13,7 +22,16 @@ const kFastImageCommodity = [
   'oil',
   'silver',
   'gold',
+  'coffee',
+  'soybeanOil',
   'platinum',
+  'wheat',
+  'corn',
+  'palladium',
+  'cotton',
+  'sugar',
+  'cocoa',
+  'copper',
 ];
 
 const kFastImageCommodityMap = {
@@ -21,5 +39,14 @@ const kFastImageCommodityMap = {
   'oil': FastImageCommodity.oil,
   'silver': FastImageCommodity.silver,
   'gold': FastImageCommodity.gold,
+  'coffee': FastImageCommodity.coffee,
+  'soybeanOil': FastImageCommodity.soybeanOil,
   'platinum': FastImageCommodity.platinum,
+  'wheat': FastImageCommodity.wheat,
+  'corn': FastImageCommodity.corn,
+  'palladium': FastImageCommodity.palladium,
+  'cotton': FastImageCommodity.cotton,
+  'sugar': FastImageCommodity.sugar,
+  'cocoa': FastImageCommodity.cocoa,
+  'copper': FastImageCommodity.copper,
 };
--- packages/images/lib/images/crypto.dart
+++ packages/images/lib/images/crypto.dart
@@ -1,28 +1,172 @@
 // Generated, do not edit
 class FastImageCrypto {
 
+  static const immutableX = 'assets/images/crypto/immutable_x.svg.vec';
+  static const iota = 'assets/images/crypto/iota.svg.vec';
+  static const daiStableCoin = 'assets/images/crypto/dai_stable_coin.svg.vec';
+  static const avalanche = 'assets/images/crypto/avalanche.svg.vec';
+  static const binance = 'assets/images/crypto/binance.svg.vec';
+  static const internetComputer = 'assets/images/crypto/internet_computer.svg.vec';
+  static const kava = 'assets/images/crypto/kava.svg.vec';
+  static const sei = 'assets/images/crypto/sei.svg.vec';
+  static const solana = 'assets/images/crypto/solana.svg.vec';
+  static const chainlink = 'assets/images/crypto/chainlink.svg.vec';
   static const bitcoinCash = 'assets/images/crypto/bitcoin_cash.svg.vec';
+  static const thorchain = 'assets/images/crypto/thorchain.svg.vec';
+  static const eos = 'assets/images/crypto/eos.svg.vec';
   static const bitcoin = 'assets/images/crypto/bitcoin.svg.vec';
+  static const okbToken = 'assets/images/crypto/okb_token.svg.vec';
+  static const tron = 'assets/images/crypto/tron.svg.vec';
+  static const injectiveProtocol = 'assets/images/crypto/injective_protocol.svg.vec';
+  static const sand = 'assets/images/crypto/sand.svg.vec';
+  static const dogecoin = 'assets/images/crypto/dogecoin.svg.vec';
+  static const shibaInu = 'assets/images/crypto/shiba_inu.svg.vec';
+  static const ethereumClassic = 'assets/images/crypto/ethereum_classic.svg.vec';
+  static const kas = 'assets/images/crypto/kas.svg.vec';
+  static const vechain = 'assets/images/crypto/vechain.svg.vec';
+  static const stacks = 'assets/images/crypto/stacks.svg.vec';
+  static const usdc = 'assets/images/crypto/usdc.svg.vec';
+  static const stellar = 'assets/images/crypto/stellar.svg.vec';
   static const ethereum = 'assets/images/crypto/ethereum.svg.vec';
+  static const matic = 'assets/images/crypto/matic.svg.vec';
+  static const optimism = 'assets/images/crypto/optimism.svg.vec';
+  static const uniswap = 'assets/images/crypto/uniswap.svg.vec';
+  static const neo = 'assets/images/crypto/neo.svg.vec';
+  static const cardano = 'assets/images/crypto/cardano.svg.vec';
+  static const tether = 'assets/images/crypto/tether.svg.vec';
+  static const aave = 'assets/images/crypto/aave.svg.vec';
+  static const hedera = 'assets/images/crypto/hedera.svg.vec';
+  static const aptos = 'assets/images/crypto/aptos.svg.vec';
+  static const lidoDao = 'assets/images/crypto/lido_dao.svg.vec';
+  static const filecoin = 'assets/images/crypto/filecoin.svg.vec';
+  static const maker = 'assets/images/crypto/maker.svg.vec';
   static const dash = 'assets/images/crypto/dash.svg.vec';
+  static const nearProtocol = 'assets/images/crypto/near_protocol.svg.vec';
+  static const polkadot = 'assets/images/crypto/polkadot.svg.vec';
+  static const algorand = 'assets/images/crypto/algorand.svg.vec';
   static const ripple = 'assets/images/crypto/ripple.svg.vec';
+  static const ton = 'assets/images/crypto/ton.svg.vec';
+  static const pancakeSwap = 'assets/images/crypto/pancake_swap.svg.vec';
   static const litecoin = 'assets/images/crypto/litecoin.svg.vec';
+  static const unusSedLeo = 'assets/images/crypto/unus_sed_leo.svg.vec';
+  static const cosmos = 'assets/images/crypto/cosmos.svg.vec';
+  static const arbitrum = 'assets/images/crypto/arbitrum.svg.vec';
+  static const monero = 'assets/images/crypto/monero.svg.vec';
+  static const tezos = 'assets/images/crypto/tezos.svg.vec';
+  static const celestia = 'assets/images/crypto/celestia.svg.vec';
+  static const ordi = 'assets/images/crypto/ordi.svg.vec';
 }
 
 const kFastImageCrypto = [
+  'immutableX',
+  'iota',
+  'daiStableCoin',
+  'avalanche',
+  'binance',
+  'internetComputer',
+  'kava',
+  'sei',
+  'solana',
+  'chainlink',
   'bitcoinCash',
+  'thorchain',
+  'eos',
   'bitcoin',
+  'okbToken',
+  'tron',
+  'injectiveProtocol',
+  'sand',
+  'dogecoin',
+  'shibaInu',
+  'ethereumClassic',
+  'kas',
+  'vechain',
+  'stacks',
+  'usdc',
+  'stellar',
   'ethereum',
+  'matic',
+  'optimism',
+  'uniswap',
+  'neo',
+  'cardano',
+  'tether',
+  'aave',
+  'hedera',
+  'aptos',
+  'lidoDao',
+  'filecoin',
+  'maker',
   'dash',
+  'nearProtocol',
+  'polkadot',
+  'algorand',
   'ripple',
+  'ton',
+  'pancakeSwap',
   'litecoin',
+  'unusSedLeo',
+  'cosmos',
+  'arbitrum',
+  'monero',
+  'tezos',
+  'celestia',
+  'ordi',
 ];
 
 const kFastImageCryptoMap = {
+  'immutableX': FastImageCrypto.immutableX,
+  'iota': FastImageCrypto.iota,
+  'daiStableCoin': FastImageCrypto.daiStableCoin,
+  'avalanche': FastImageCrypto.avalanche,
+  'binance': FastImageCrypto.binance,
+  'internetComputer': FastImageCrypto.internetComputer,
+  'kava': FastImageCrypto.kava,
+  'sei': FastImageCrypto.sei,
+  'solana': FastImageCrypto.solana,
+  'chainlink': FastImageCrypto.chainlink,
   'bitcoinCash': FastImageCrypto.bitcoinCash,
+  'thorchain': FastImageCrypto.thorchain,
+  'eos': FastImageCrypto.eos,
   'bitcoin': FastImageCrypto.bitcoin,
+  'okbToken': FastImageCrypto.okbToken,
+  'tron': FastImageCrypto.tron,
+  'injectiveProtocol': FastImageCrypto.injectiveProtocol,
+  'sand': FastImageCrypto.sand,
+  'dogecoin': FastImageCrypto.dogecoin,
+  'shibaInu': FastImageCrypto.shibaInu,
+  'ethereumClassic': FastImageCrypto.ethereumClassic,
+  'kas': FastImageCrypto.kas,
+  'vechain': FastImageCrypto.vechain,
+  'stacks': FastImageCrypto.stacks,
+  'usdc': FastImageCrypto.usdc,
+  'stellar': FastImageCrypto.stellar,
   'ethereum': FastImageCrypto.ethereum,
+  'matic': FastImageCrypto.matic,
+  'optimism': FastImageCrypto.optimism,
+  'uniswap': FastImageCrypto.uniswap,
+  'neo': FastImageCrypto.neo,
+  'cardano': FastImageCrypto.cardano,
+  'tether': FastImageCrypto.tether,
+  'aave': FastImageCrypto.aave,
+  'hedera': FastImageCrypto.hedera,
+  'aptos': FastImageCrypto.aptos,
+  'lidoDao': FastImageCrypto.lidoDao,
+  'filecoin': FastImageCrypto.filecoin,
+  'maker': FastImageCrypto.maker,
   'dash': FastImageCrypto.dash,
+  'nearProtocol': FastImageCrypto.nearProtocol,
+  'polkadot': FastImageCrypto.polkadot,
+  'algorand': FastImageCrypto.algorand,
   'ripple': FastImageCrypto.ripple,
+  'ton': FastImageCrypto.ton,
+  'pancakeSwap': FastImageCrypto.pancakeSwap,
   'litecoin': FastImageCrypto.litecoin,
+  'unusSedLeo': FastImageCrypto.unusSedLeo,
+  'cosmos': FastImageCrypto.cosmos,
+  'arbitrum': FastImageCrypto.arbitrum,
+  'monero': FastImageCrypto.monero,
+  'tezos': FastImageCrypto.tezos,
+  'celestia': FastImageCrypto.celestia,
+  'ordi': FastImageCrypto.ordi,
 };
