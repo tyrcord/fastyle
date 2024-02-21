c498a152284a26625250d8927438a409932091ec
Thibault Zanini <thibaultzanini@gmail.com>
2024-02-21 15:58:06 +0100

    feat: add analytics helpers

--- packages/firebase/lib/logic/helpers/analytics.helper.dart
+++ packages/firebase/lib/logic/helpers/analytics.helper.dart
@@ -1,15 +1,51 @@
 import 'package:fastyle_core/fastyle_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:t_helpers/helpers.dart';
 import 'package:tbloc/tbloc.dart';
 
-void logFastBlocAnalyticsEvent(BlocAnalyticsEvent event) {
+void logAnalyticsEvent({
+  required String name,
+  Map<String, dynamic>? parameters,
+}) {
   final analytics = FirebaseAnalytics.instance;
+  final sanitizedParams = _sanitizeAnalyticsParams(parameters);
+
+  analytics.logEvent(name: name, parameters: sanitizedParams);
+}
+
+void logFastBlocAnalyticsEvent(BlocAnalyticsEvent event) {
   final parameters = event.parameters;
   final type = event.type;
 
-  // Log the export results analytics event.
   if (type is FastBlocAnalyticsEvent) {
-    final eventName = type.snakeCase;
-    analytics.logEvent(name: eventName, parameters: parameters);
+    logAnalyticsEvent(name: type.snakeCase, parameters: parameters);
   }
+}
+
+Map<String, dynamic>? _sanitizeAnalyticsParams(
+  Map<String, dynamic>? originalMap,
+) {
+  if (originalMap == null) return null;
+
+  return Map<String, dynamic>.fromEntries(
+    originalMap.entries.where((entry) {
+      final keyLength = entry.key.length;
+
+      if (keyLength > 40) {
+        debugLog('The key "${entry.key}" is too long and will be discarded.');
+
+        return false;
+      }
+
+      return entry.value != null;
+    }).map((entry) {
+      var value = entry.value;
+
+      if (value is! String && value is! num) {
+        value = value.toString();
+      }
+
+      return MapEntry<String, dynamic>(entry.key, value);
+    }),
+  );
 }
